2b03c35d31540ffb3d2ee8129fb87212fc1dd2cc
Antonin Courtin <antonin.courtin.1@ens.etsmtl.ca>
2018-11-26 18:20:40 -0500

    Creation fenetre de base

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import gui.PerspectiveImageView;
+import model.Perspective;
+
 public class Main {
 
-    public static void Main(String[] args) {
+    public static void main(String[] args) {
+
+        Perspective model = new Perspective();
+        PerspectiveImageView piw = new PerspectiveImageView(model);
         //TODO: Run the app
     }
 }
--- src/gui/PerspectiveImageView.java
+++ src/gui/PerspectiveImageView.java
@@ -1,18 +1,55 @@
 package gui;
 
+import model.Perspective;
+
+import javax.swing.*;
+import java.awt.*;
 import java.util.Observable;
 import java.util.Observer;
-
-import javax.swing.JFrame;
 
 public class PerspectiveImageView extends JFrame implements Observer {
 	
 	private PanelImage panelImg;
 	private PanelCommands panelCommands;
-	
+	private Perspective model;
+
+	private static final long serialVersionUID = 1L;
+	private static final String TITRE_FENETRE = "Devoir 2 - Log121";
+	private static final Dimension DIMENSION = new Dimension(500, 500);
+
 	@Override
 	public void update(Observable o, Object arg) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	public PerspectiveImageView (Perspective model){
+		this.model=model;
+		initFrame();
+	}
+
+	private void initFrame(){
+		panelImg=new PanelImage();
+		PanelCommands pc = new PanelCommands();
+		PanelZoom pz=new PanelZoom();
+		PanelTranslation pt= new PanelTranslation();
+		this.add(panelImg);
+		this.add(pc);
+		this.add(pz);
+		this.add(pt);
+
+
+		// Faire en sorte que le X de la fen�tre ferme la fen�tre
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setTitle(TITRE_FENETRE);
+		setSize(DIMENSION);
+		// Rendre la fen�tre visible
+		setVisible(true);
+		// Mettre la fen�tre au centre de l'�cran
+		setLocationRelativeTo(null);
+		// Emp�cher la redimension de la fen�tre
+		setResizable(false);
+
+
 	}
 }
