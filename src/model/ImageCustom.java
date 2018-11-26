270517f7f39202c9d380d6587e0b8da5822f6f30
Grenier-Turcotte <jeremy.grenier-turcotte.1@ens.etsmtl.ca>
2018-11-26 13:07:50 -0500

    squelette classes modele

---
+++ src/model/ImageCustom.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class ImageCustom implements Serializable {
+	private PerspectiveImage currentPerspective;
+	
+	public ImageCustom(List<PerspectiveImage> perspectives){
+		
+	}
+	
+	public void changePerspective(int index){
+		
+	}
+}
--- src/model/PerspectiveImage.java
+++ src/model/PerspectiveImage.java
@@ -1,4 +1,26 @@
 package model;
 
-public class PerspectiveImage {
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+
+public class PerspectiveImage extends Observable{
+	private String iconPath;
+	private double zoomLevel;
+	private Point position;
+	private List<Observer> observers;
+	
+	public BufferedImage getImage(){
+		return null;
+	}
+	
+	public void translate(int x, int y){
+		
+	}
+	
+	public void setZoom(double zoom){
+		
+	}
 }
