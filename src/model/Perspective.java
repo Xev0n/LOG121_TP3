faeba13636dbd4a22377170f1f5d16a68501f2e4
PhilippeGervais <philippe-gervais.bang-nyamba.1@ens.etsmtl.ca>
2018-12-02 04:45:38 -0500

    Ajout des méthodes du model

--- src/model/Perspective.java
+++ src/model/Perspective.java
@@ -1,6 +1,9 @@
 package model;
 
 import javax.imageio.ImageIO;
+
+import observer.AbstractObservable;
+
 import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -9,15 +12,17 @@ import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
-public class Perspective extends Observable{
+public class Perspective extends AbstractObservable{
 	private String iconPath;
 	private double zoomLevel;
 	private Point position;
-	private List<Observer> observers;
+	private int width = 500;
+	private int height = 400;
 
 
 	public Perspective(){
 		iconPath="src/ressources/img.jpg";
+		position = new Point();
 
 	}
 	
@@ -46,5 +51,67 @@ public class Perspective extends Observable{
 	public String[] getPerspectivesNames(){
 		String[] names = {"Perspective 1", "Perspective 2"};
 		return names;
+	}
+
+	public boolean deZoomer(double increment) {
+		if(width * height == 0)
+			return false;
+		this.width -= increment;
+		this.height -= increment;
+		position.x += increment / 2;
+		position.y += increment / 2;
+		notifier();
+		return true;
+	}
+
+	public boolean zoomer(double increment) {
+		if(width * height == 0)
+			return false;
+		this.width += increment;
+		this.height += increment;
+		position.x -= increment / 2;
+		position.y -= increment / 2;
+		notifier();
+		return true;
+	}
+
+	public int getWidth() {
+		return this.width ;
+	}
+
+	public int getHeight() {
+		return this.height;
+	}
+
+	public int getY() {
+		return position.y;
+	}
+
+	public int getX() {
+		return position.x;
+	}
+
+	public boolean translateDown(int increment) {
+		position.y += increment;
+		notifier();
+		return true;
+	}
+
+	public boolean translateUp(int increment) {
+		position.y -= increment;
+		notifier();
+		return true;
+	}
+
+	public boolean translateRight(int increment) {
+		position.x += increment;
+		notifier();
+		return true;
+	}
+
+	public boolean translateLeft(int increment) {
+		position.x -= increment;
+		notifier();
+		return true;
 	}
 }
